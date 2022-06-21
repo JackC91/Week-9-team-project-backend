e18e42ff63aa35be5f69ce2c766e607e3d92546d
halil-coskun <halilcoskun14@gmail.com>
2022-06-21 13:50:31 +0100

    first attempt at post req

--- routes/routes.js
+++ routes/routes.js
@@ -21,7 +21,11 @@ router.get("/computational_thinking", async function(req, res){
 //route URI to POST to named table
 router.post("/computational_thinking", async function(req, res){
     const content = req.body.user_id;
-    res.json({content});
+    async function postToList() {
+       response = await query(`INSERT INTO computational_thinking (user_id) VALUES (${content});`);
+    };
+    res.json({ success: true,
+               payload: content});
     console.log(content)
     //res.json({})
 })
