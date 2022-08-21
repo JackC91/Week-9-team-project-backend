345ffaefdef751abb4ca9c1a23dbfac03121ddb6
JackC91 <jackjcherry@hotmail.co.uk>
2022-08-21 12:58:51 +0100

    updated db file to reflect deployed backend

--- db/index.js
+++ db/index.js
@@ -1,14 +1,21 @@
 import pg from "pg"; 
 
 // pools will use environment variables for connection information to keep credentials off of GitHub
- const pool = new pg.Pool({
-    user: process.env.PGUSER,
-    host:process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,   
-    ssl:{rejectUnauthorized:false} 
-})
+//  const pool = new pg.Pool({
+//     user: process.env.PGUSER,
+//     host:process.env.PGHOST,
+//     database: process.env.PGDATABASE,
+//     password: process.env.PGPASSWORD,
+//     port: process.env.PGPORT,   
+//     ssl:{rejectUnauthorized:false} 
+// })
+
+export const pool = new pg.Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
+});
 
 // Saves writing pool.query each time pool is used.
 export function query(text, params, callback){
