f2c25b0c59cdfbd433b8c5222eecbf24117f3e44
unknown <jackjcherry@hotmail.co.uk>
2022-08-31 11:41:43 +0100

    port variable updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import router from "./routes/routes.js";
 
 const app = express();
-const port = 3005;
+const port = process.env.PORT || 3005;
 
 app.use(cors());
 app.use(express.json());
