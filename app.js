736be92098857bcf715b12941e0b7fe26360baf6
unknown <jackjcherry@hotmail.co.uk>
2022-08-31 12:08:17 +0100

    cors workaround

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const port = process.env.PORT || 3005;
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use("/", router);
 
 app.listen(port, () => {
